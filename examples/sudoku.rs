a87b1c30e9456be51ee650b3e8c5ecc95b9dbb22
Mason Smith <masonium@gmail.com>
2016-07-09 02:53:40 -0700

    Adds list of sudoku puzzles for testing / benches

--- examples/sudoku.rs
+++ examples/sudoku.rs
@@ -1,54 +1,58 @@
 extern crate exact_cover;
 
 use exact_cover::instances::sudoku::{SudokuSolver, sudoku_solver, solution_as_matrix};
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{stdin};
 
-fn read_sudoku() -> Option<SudokuSolver> {
-    let br = BufReader::new(stdin());
-    let mut v: Vec<usize> = Vec::new();
-    for line in br.lines() {
-        if line.is_err() {
-            break;
-        }
-        for d in line.unwrap().chars().filter_map(|x| x.to_digit(10)) {
-            v.push(d as usize)
-        }
+fn read_sudoku() -> (String, Option<SudokuSolver>) {
+    let mut line = String::new();
+    if stdin().read_line(&mut line).is_err() {
+        return (line, None)
     }
 
+    let v: Vec<usize> = line.chars().map(|x| if x == '.' { '0' } else { x })
+        .filter_map(|x| x.to_digit(10))
+        .map(|x| x as usize).collect();
+
     if v.len() != 81 {
-        return None
+        return (line, None)
     }
 
     // This crate also contains implemtations for specific problems.
     // For instance, `sudoku_solver()` takes a 1-d slice of usizes
     // representing a sudoku problem and returns a `Solver` object
     // that will generate solutions.
-    sudoku_solver(&v).ok()
+    (line, sudoku_solver(&v).ok())
 }
 
 /// Read a sudoku from the command line and solve.
 fn main() {
 
-    let s  = read_sudoku();
-    let sol = s.unwrap().first_solution();
-    match sol {
-        Some(a) => {
-            let x = solution_as_matrix(9, &a);
-            for (i, v) in x.iter().enumerate() {
-                if i == 3 || i == 6 {
-                    println!("───┼───┼───");
-                }
-                for (j, c) in v.iter().enumerate() {
-                    if j == 3 || j == 6 {
-                        print!("│");
+    loop {
+        let (s, solver) = read_sudoku();
+        if solver.is_none() {
+            break;
+        }
+        let sol = solver.unwrap().first_solution();
+        match sol {
+            Some(a) => {
+                print!("{}", s);
+                let x = solution_as_matrix(9, &a);
+                for (i, v) in x.iter().enumerate() {
+                    if i == 3 || i == 6 {
+                        println!("───┼───┼───");
                     }
-                    print!("{}", c);
+                    for (j, c) in v.iter().enumerate() {
+                        if j == 3 || j == 6 {
+                            print!("│");
+                        }
+                        print!("{}", c);
+                    }
+                    println!("");
                 }
-                println!("");
+            },
+            None => {
+                println!("No solution found.");
             }
-        },
-        None => {
-            println!("No solution found.");
         }
     }
 }
